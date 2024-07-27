516f1a3a0c8a9ee0552ddbd59a3c48f5b82bba4a
AndreLuizOenning <andreluizoenning611@gmail.com>
2024-07-27 10:32:07 -0300

    bug  - retenção

--- App/src/firebase/login.tsx
+++ App/src/firebase/login.tsx
@@ -1,13 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, initializeAuth, signInWithEmailAndPassword } from "firebase/auth";
+import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
+import { getReactNativePersistence } from "@firebase/auth/distribution/rn/index.js";
 
 
 
 export default async function FireLogin(email: string, password:string){
     const auth = getAuth();
-    const retencao = initializeAuth(firebaseApp,{
-        persistence:getReactNativePersistence(ReactNativeAsyncStorage),
-    })
+    //const retencao = initializeAuth(firebaseApp,{
+      //  persistence:getReactNativePersistence(ReactNativeAsyncStorage),
+    //})
     return await signInWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
         const user = userCredential.user;
